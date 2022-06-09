3a8f46097370d4984150730b182c3b95de0a94bb
george <heohii.pustovit@ava.codes>
2022-06-09 20:00:44 +0300

    [BT-4] remove extra symbol

--- src/components/statistic/ListItem.tsx
+++ src/components/statistic/ListItem.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
-import { formattingNumber } from '../../utils/formattingNumber`';
+import { formattingNumber } from '../../utils/formattingNumber';
 import { listForRangeItem } from './List';
 
 type propsType = {
