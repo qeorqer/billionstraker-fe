d1f53f4a43d6f99c1673125bc6b1d0a99edd5bc7
george <heohii.pustovit@ava.codes>
2022-07-16 13:02:46 +0300

    [BT-7] add the same swiping logic for the categories

--- src/components/Categories/CategoriesList/index.tsx
+++ src/components/Categories/CategoriesList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, Col, Row } from 'react-bootstrap';
+import { Card, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { categoryType } from 'types/category.type';
 
@@ -17,18 +18,38 @@ const CategoriesList: React.FC<propTypes> = ({ categories }) => {
 
   return (
     <Row className="mb-3  justify-content-center d-flex">
-      {categories.map((category) => (
-        <Col xs={12} lg={3} md={4} sm={6} className="mb-3">
-          <Card>
-            <Card.Body>
-              <Card.Title>{category.name}</Card.Title>
-              <Card.Text>{`${t('category type')}: ${t(
-                category.categoryType,
-              )}`}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={1}
+        className="mb-3  justify-content-center d-flex px-2"
+        breakpoints={{
+          450: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+      >
+        {categories.map((category) => (
+          <SwiperSlide className="mb-3" key={category._id}>
+            <Card>
+              <Card.Body>
+                <Card.Title>{category.name}</Card.Title>
+                <Card.Text>{`${t('category type')}: ${t(
+                  category.categoryType,
+                )}`}</Card.Text>
+              </Card.Body>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Row>
   );
 };
