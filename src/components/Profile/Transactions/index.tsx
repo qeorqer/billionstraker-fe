32d9f2a0e73c43d2b456f319be45fc1d49f3114b
george <heohii.pustovit@ava.codes>
2022-07-16 14:00:31 +0300

    [BT-7] add infinite scroll

--- src/components/Profile/Transactions/index.tsx
+++ src/components/Profile/Transactions/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from 'hooks/react-redux.hook';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import {
   getAllUserTransactions,
@@ -27,26 +28,29 @@ const Transactions = () => {
   const LIMIT = 10;
   const [numberToSkip, setNumberToSkip] = useState<number>(LIMIT);
   const [categoriesToShow, setCategoriesToShow] = useState<string[]>([]);
-  const [shownTransactionsTypes, setShownTransactionsTypes] =
-    useState<transactionTypesToShowType>('all transactions');
+  const [shownTransactionsTypes, setShownTransactionsTypes] = useState<transactionTypesToShowType>('all transactions');
   const [balancesToShow, setBalancesToShow] = useState<string[]>([]);
 
-  const getAllTransactions = () => {
-    dispatch(
-      getAllUserTransactions({
-        limit: LIMIT,
-        numberToSkip: numberToSkip,
-        filteringOptions: {
-          shownTransactionsTypes,
-          categoriesToShow,
-          balancesToShow,
-        },
-      }),
-    );
-    setNumberToSkip(LIMIT + numberToSkip);
+  const handleLoadMore = () => {
+    if(!isTransactionsloading){
+      dispatch(
+        getAllUserTransactions({
+          limit: LIMIT,
+          numberToSkip: numberToSkip,
+          filteringOptions: {
+            shownTransactionsTypes,
+            categoriesToShow,
+            balancesToShow,
+          },
+        }),
+      );
+      setNumberToSkip(LIMIT + numberToSkip);
+    }
   };
 
   useEffect(() => {
+    setNumberToSkip(LIMIT);
+
     dispatch(resetTransactions());
     dispatch(
       getAllUserTransactions({
@@ -70,7 +74,7 @@ const Transactions = () => {
   }
 
   return (
-    <div className="mt-4">
+    <div className='mt-4'>
       {!(
         !numberOfTransactions &&
         shownTransactionsTypes === 'all transactions' &&
@@ -79,9 +83,9 @@ const Transactions = () => {
         !isTransactionsloading
       ) && (
         <>
-          <p className="text-center fw-bold fs-4">{t('apply filters')}</p>
-          <Row className="mb-3">
-            <Col xs="12" sm="4">
+          <p className='text-center fw-bold fs-4'>{t('apply filters')}</p>
+          <Row className='mb-3'>
+            <Col xs='12' sm='4'>
               <Form.Select
                 onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
                   setShownTransactionsTypes(
@@ -90,43 +94,43 @@ const Transactions = () => {
                 }
               >
                 {transactionTypesToShow &&
-                  transactionTypesToShow.map(
-                    (type: transactionTypesToShowType, index) => (
-                      <option key={index} value={type}>
-                        {t(type)}
-                      </option>
-                    ),
-                  )}
+                transactionTypesToShow.map(
+                  (type: transactionTypesToShowType, index) => (
+                    <option key={index} value={type}>
+                      {t(type)}
+                    </option>
+                  ),
+                )}
               </Form.Select>
             </Col>
-            <Col xs="12" sm="4">
+            <Col xs='12' sm='4'>
               <Form.Select
                 onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
                   setCategoriesToShow(
                     e.target.value === 'all' ? [] : [e.target.value],
                   );
                 }}
-                className="my-2 my-sm-0"
+                className='my-2 my-sm-0'
                 disabled={shownTransactionsTypes === 'exchange'}
               >
-                <option value="all">{t('show all')}</option>
+                <option value='all'>{t('show all')}</option>
                 {categories &&
-                  categories
-                    .filter((category: categoryType) => {
-                      if (shownTransactionsTypes === 'all transactions') {
-                        return category;
-                      }
+                categories
+                .filter((category: categoryType) => {
+                  if (shownTransactionsTypes === 'all transactions') {
+                    return category;
+                  }
 
-                      return category.categoryType === shownTransactionsTypes;
-                    })
-                    .map((category: categoryType) => (
-                      <option key={category._id} value={category.name}>
-                        {category.name}
-                      </option>
-                    ))}
+                  return category.categoryType === shownTransactionsTypes;
+                })
+                .map((category: categoryType) => (
+                  <option key={category._id} value={category.name}>
+                    {category.name}
+                  </option>
+                ))}
               </Form.Select>
             </Col>
-            <Col xs="12" sm="4">
+            <Col xs='12' sm='4'>
               <Form.Select
                 onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
                   setBalancesToShow(
@@ -135,13 +139,13 @@ const Transactions = () => {
                 }
                 disabled={shownTransactionsTypes === 'exchange'}
               >
-                <option value="all">{t('show all')}</option>
+                <option value='all'>{t('show all')}</option>
                 {balances &&
-                  balances.map((balance: balanceType) => (
-                    <option key={balance._id} value={balance.name}>
-                      {balance.name}
-                    </option>
-                  ))}
+                balances.map((balance: balanceType) => (
+                  <option key={balance._id} value={balance.name}>
+                    {balance.name}
+                  </option>
+                ))}
               </Form.Select>
             </Col>
           </Row>
@@ -149,26 +153,20 @@ const Transactions = () => {
       )}
       {numberOfTransactions ? (
         <>
-          <p className="text-center fw-bold fs-4">{t('Your transactions')}</p>
-          {transactions.map((transaction) => (
-            <Transaction key={transaction._id} transaction={transaction} />
-          ))}
-
-          {isTransactionsloading && <Loader />}
-
-          {numberToSkip <= numberOfTransactions && (
-            <Button
-              disabled={isTransactionsloading}
-              variant="warning"
-              className="mt-4 mx-auto d-block text-white"
-              onClick={getAllTransactions}
-            >
-              {t('Load more')}
-            </Button>
-          )}
+          <p className='text-center fw-bold fs-4'>{t('Your transactions')}</p>
+          <InfiniteScroll
+            loadMore={handleLoadMore}
+            hasMore={numberToSkip <= numberOfTransactions}
+            loader={<Loader />}
+          >
+            {transactions.map((transaction) => (
+              <Transaction key={transaction._id} transaction={transaction} />
+            ))}
+          </InfiniteScroll>
         </>
       ) : (
-        <div className="d-flex justify-content-center align-items-center h-100 fw-bold my-3 mt-3">
+        <div
+          className='d-flex justify-content-center align-items-center h-100 fw-bold my-3 mt-3'>
           <p>{t('There is no transactions yet')}</p>
         </div>
       )}
