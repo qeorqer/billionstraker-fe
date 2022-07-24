f6fec143d448020e7de86ffb41235c6ea6ecade4
george <heohii.pustovit@ava.codes>
2022-07-24 11:31:00 +0300

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,44 @@
+let CACHE_NAME = 'billionstraker-pwa'
+;
+const urlsToCache = [
+'/home',
+'/createTransaction',
+];
+
+self.addEventListener('install', function(event) {
+// Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = ['billionstraker-pwa'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
