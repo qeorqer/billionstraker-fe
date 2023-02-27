0cea853c77609398e931fa54c3f033f69c133177
george <heohii.pustovit@ava.codes>
2023-02-27 20:33:12 +0200

    [BT-10] Add range detail

--- src/components/Statistics/RangeStatistic/index.tsx
+++ src/components/Statistics/RangeStatistic/index.tsx
@@ -1,8 +1,8 @@
-import React, { Dispatch, FC, SetStateAction } from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import React, { Dispatch, FC, SetStateAction, useState } from 'react';
+import { Button, Col, Row, Form } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 //@ts-ignore
-//todo: learn how to work when there is no ts for library
+//todo: This library has an awful typing, but check it once in a while
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
 
 import Loader from 'components/Loader';
@@ -36,17 +36,20 @@ const RangeStatistic: FC<propsType> = ({
   const { lang, user } = useAppSelector(userData);
   const { isStatisticsForBalanceLoading } = useAppSelector(statisticData);
   const { balances } = useAppSelector((state) => state.balanceData);
+  const [dateRangeMaxDetail, setDateRangeMaxDetail] = useState<
+    'year' | 'month'
+  >('year');
 
   const handleCreateTransaction = () => push('createTransaction');
 
   return (
     <>
       <Row className="d-flex justify-content-center text-center white-space-nowrap">
-        <Col xs={12} md={6} lg={3}>
-          <p className="fs-5 mb-1 fw-bold text-center">{t('Select range')}:</p>
+        <Col xs={12} sm={6} lg={4} className="max-width-220">
+          <p className="mb-1 fw-bold text-center">{t('Select range')}:</p>
           <DateRangePicker
             onChange={setMonthsRange}
-            maxDetail="year"
+            maxDetail={dateRangeMaxDetail}
             value={monthsRange}
             locale={lang}
             calendarIcon={null}
@@ -59,8 +62,24 @@ const RangeStatistic: FC<propsType> = ({
             onFocus={(e: any) => (e.target.readOnly = true)}
           />
         </Col>
-        <Col xs={12} md={6} lg={3} className="max-width-220">
-          <p className="fs-5 mb-1 fw-bold text-center white-space-nowrap">
+        <Col
+          xs={12}
+          sm={6}
+          lg={4}
+          className="max-width-220 mx-md-2 mt-2 mt-sm-0">
+          <p className="mb-1 fw-bold text-center">{t('range detail')}:</p>
+          <Form.Control
+            as="select"
+            value={dateRangeMaxDetail}
+            onChange={(event) =>
+              setDateRangeMaxDetail(event.target.value as 'year' | 'month')
+            }>
+            <option value="year">{t('year')}</option>
+            <option value="month">{t('month')}</option>
+          </Form.Control>
+        </Col>
+        <Col xs={12} lg={4} className="max-width-220 mt-2 mt-sm-0">
+          <p className="mb-1 fw-bold text-center white-space-nowrap">
             {t('select balance')}
           </p>
           <CustomSelect
@@ -103,8 +122,7 @@ const RangeStatistic: FC<propsType> = ({
           <Button
             variant="warning"
             className="w300Px text-white"
-            onClick={handleCreateTransaction}
-          >
+            onClick={handleCreateTransaction}>
             {t('create transaction')}
           </Button>
         </div>
